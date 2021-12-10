d4fd1a4f228d9c23df0570bfad965e9a77faa980
2pichar <77556662+2pichar@users.noreply.github.com>
2021-12-10 11:13:29 -0800

    Started on main loop

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,21 @@
 import * as rsa from './rsa';
 import * as hash from './hash';
-import * as user from './user';
+import {input} from './user';
 
-const ui = new user.UI();
+async function main(): void{
+    while (true){
+        console.log("Welcome to RSA-ex in TS!");
+        console.log("This is an example of the RSA encryption method that is used today in the Internet to encrypt data.")
+        console.log("What do you want to do?")
+        console.log("Generate public/private keys (g)");
+        console.log("Encrypt a message (e)");
+        console.log("Decrypt a message (d)");
+        try {
+            let choice = await input(">");
+        } catch(e){
+            
+        };
+    }
+}
+
+main();
