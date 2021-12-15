6217522dd403287fb0a9aae89ca61922881ae8c0
2pichar <77556662+2pichar@users.noreply.github.com>
2021-12-15 12:29:43 -0800

    updated hash functions to use Buffer

--- src/ts/hash.ts
+++ src/ts/hash.ts
@@ -1,27 +1,21 @@
-const MASK = 2**8;
-const CHAR = 4;
-function number(bits){
-	return +(`0x${bits}`);
-}
-function hash(s){
-	let hexStr = '';
-	for(let i = 0; i < s.length; i++){
-		hexStr += (s.charCodeAt(i) ^ MASK).toString(16).padStart(CHAR, '0');
-	}
-	return hexStr;
+function encode(s: str): str{
+	let buf: Buffer = Buffer.alloc(s.length);
+	buf.fill(0);
+	buf.write(s, 0, s.length, 'utf8');
+	return buf.toString('hex');
 }
 
-function unhash(h){
-	let mod = h.length % CHAR;
-	h.padStart(CHAR * ((h.length - mod)/CHAR + (mod == 0 ? 0 : 0)), '0');
-	let str = '';
-	for(let i = 0; i < h.length; i+= CHAR){
-		let n = number(h.slice(i, i+CHAR));
-		let char = n ^ MASK;
-		str += String.fromCharCode(char);
-	}
-	return str;
+function decode(s: str): str{
+	return Buffer.from(s, 'hex').toString('utf8');
 }
 
-export default hash;
-export {hash, unhash, number};
+function decimal(s: str): int64{
+	return BigInt(Number.parseInt(s, 16));
+}
+
+function hex(n: int | int64): str{
+	return n.toString(16);
+}
+
+export default encode;
+export {encode, decode, decimal, hex};
