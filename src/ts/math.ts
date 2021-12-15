9a3528a51df0c15dfd4427036918db05692cb66f
2pichar <77556662+2pichar@users.noreply.github.com>
2021-12-15 12:43:45 -0800

    removed console.logs

--- src/ts/math.ts
+++ src/ts/math.ts
@@ -1,8 +1,7 @@
 import './types.js';
 import {checkPrimeSync as checkPrime} from 'crypto';
 
-function modularExponentiation(x: int64, y: int64, m: int64): int64
-{
+function modularExponentiation(x: int64, y: int64, m: int64): int64{
     // Initialize result
     let res = 1n;
  
@@ -29,16 +28,13 @@ function modularExponentiation(x: int64, y: int64, m: int64): int64
     return res;
 }
 function modularInverse(a: int | int64, m: int | int64): int | int64 {
-  console.log('modularInverse()');
   // validate inputs
   [a, m] = [int64(a), int64(m)]
   if (Number.isNaN(a) || Number.isNaN(m)) {
-    console.log('NaN1');
     return NaN // invalid input
   }
   a = (a % m + m) % m
   if (!a || m < 2) {
-    console.log('NaN2');
     return NaN // invalid input
   }
   // find the gcd
@@ -46,11 +42,9 @@ function modularInverse(a: int | int64, m: int | int64): int | int64 {
   let b: int64 = m
   while(b) {
     [a, b] = [b, a % b];
-    console.log(a, b);
     s.push({a, b})
   }
   if (a !== 1n) {
-    console.log('NaN3');
     return NaN // inverse does not exists
   }
   // find the inverse
@@ -63,40 +57,32 @@ function modularInverse(a: int | int64, m: int | int64): int | int64 {
 }
   
 function generatePrime(bits: int | int64): int64 {
-  console.log('generatePrime()');
     bits = int64(bits);
     let prime: int64 = getRandomPrime(2n ** bits, 2n ** (bits + 1n));
     return prime;
 }
 
 function generateCoprime(n: int | int64): int64 {
-  console.log('generateCoprime()');
   n = int64(n);
   return n - 1n;
 }
 
 function getRandomPrime(min: int | int64, max: int | int64): int64 {
-  console.log('getRandomPrime()');
   let num: int64 = getRandomInt(min, max);
   if(!(num & 1n)){
     num++;
   }
-  console.log(num);
   while (!checkPrime(num)) {
       num+=2n;
-      console.log(num);
   }
-  console.log(num);
   return num;
 }
 
 function getRandomInt(min: int | int64, max: int | int64): int64 {
-  console.log('getRandomInt()');
   return int64(Math.floor(int(Math.random() * int((int64(max) - int64(min) + 1n) + int64(min)))));
 }
 
 function isPrime(num: int64): bool {
-  console.log('isPrime()');
   if (num <= 3n) return num > 1n;
   
   if ((num % 2n === 0n) || (num % 3n === 0n)) return false;
@@ -113,14 +99,12 @@ function isPrime(num: int64): bool {
   }
 
 function lcm(a: int | int64, b: int | int64): int64 {
-  console.log('lcm()');
   a = int64(a);
   b = int64(b);  
   return int64(a * b / gcd(a, b));
 }
 
 function gcd(a: int | int64, b: int | int64): int64 {
-  console.log('gcd()');
   a = int64(a);
   b = int64(b);
   if (b === 0n) {
